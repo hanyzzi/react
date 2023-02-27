b08aa2ebb7a77635a6aeaa824b3f4e1b24f855ad
Youngbin <comgod512@naver.com>
2023-02-27 17:51:41 +0900

    빈화면 출력 오류 수정

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import PostListPage from './pages/PostListPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import WritePage from './pages/WritePage';
-import PostPage from './pages/PostPage';
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
+import PostListPage from "./pages/PostListPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
+import PostPage from "./pages/PostPage";
 
 const App = () => {
   return (
-    <>
-      <Route component={PostListPage} path={['/@:username', '/']} exact />
-      <Route component={LoginPage} path="/login" />
-      <Route component={RegisterPage} path="/register" />
-      <Route component={WritePage} path="/write" />
-      <Route component={PostPage} path="/@:username/:postId" />
-    </>
+    <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/PostListPage/:id" element={<PostListPage />} />
+        <Route path="/PostPage/:username/:postId" element={<PostPage />} />
+        <Route path="/write" element={<WritePage />} />
+      </Routes>
   );
 };
 export default App;
